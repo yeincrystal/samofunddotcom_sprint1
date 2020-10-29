a6a1a8773b2cc4fbfec28f0ea76b4862b6bac813
yein <yeincrystal@gmail.com>
2020-10-29 15:24:37 +0900

    update a search function

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     path('<int:port_id>/', views.detail, name='detail'),
     path('new/', views.new, name="new"),
     path('create/', views.create, name="create"),
+    path('search/', views.search, name="search"),
     path('<int:port_id>/edit', views.edit, name='edit'),
     path('<int:port_id>/update', views.update, name='update'),
     path('<int:port_id>/delete', views.delete, name="delete"),
--- portfolio/views.py
+++ portfolio/views.py
@@ -23,6 +23,25 @@ def create(request):
     port = Portfolios(author=author, body=body, created_at=timezone.now()) 
     port.save()
     return redirect('portfolio:detail', port_id=port.id)
+#-------------------------------------SEARCH FUNCTION-----------------------------
+#Question) index함수 안에 만들어야하나? 
+def search(request):
+    if request.method == "POST":
+        author = request.POST["fund"] #Question) input tag의 name을 넣는게 맞는지 확인
+        #검색결과에 일치하는 author이 있는지 확인 
+        fund = Portfolios.objects.get(author = author)
+        if not fund:
+            body = request.POST["fund"]
+            #검색결과와 일치하는 body가 있는지 확인
+            fund = Portfolios.objects.get(body = body)
+        context = {'fund':fund}
+
+        #찾은 검색결과랑 전체 리스트 전부 보여주기         
+        return render(request, "portfolio/index.html", context)
+    #get방식일 때는 다시 index함수로 가도록 하기 
+    else:
+        return render('portfolio:index')
+#----------------------------------------------------------------------------------
 
 def edit(request, port_id):
     port = Portfolios.objects.get(id=port_id)
